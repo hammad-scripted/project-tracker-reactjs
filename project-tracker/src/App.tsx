1eca0e71684727f7dc65c2bc763c17e67840e53c
Mohammad Hammad <hammad.scripted@gmail.com>
2026-09-11 00:32:28 +0530

    feat: added initial project structure

---
+++ project-tracker/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Welcome to Project Tracker
+      </h1>
+    </div>
+  );
+};
+
+export default App;
